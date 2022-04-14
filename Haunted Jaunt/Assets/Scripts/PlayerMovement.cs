ebe69c15468559ac2d9617fb17376f4a36a58159
Olivia Goldstein <oliviagoldstein@You-betta-watch-out.local>
2022-04-13 19:55:53 -0700

    implemented angle using dot product

--- Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
+++ Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
+    public TextMeshProUGUI currentAngle;
 
     Animator m_Animator;
     Vector3 m_Movement;
+    Vector3 startVector;
+    Vector3 currentVector;
+    private float angle;
+    private float radians;
+    private float dotProduct;
+    private float startMagnitude;
+    private float currentMagnitude;
+    private float magnitudes;
     Quaternion m_Rotation = Quaternion.identity;
     Rigidbody m_Rigidbody;
     AudioSource m_AudioSource;
@@ -17,6 +27,8 @@ public class PlayerMovement : MonoBehaviour
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
         m_AudioSource = GetComponent<AudioSource>();
+        startVector = m_Rigidbody.transform.position;
+        startMagnitude = startVector.magnitude;
     }
 
     // Update is called once per frame
@@ -47,10 +59,24 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        currentVector = m_Rigidbody.transform.position;
+        currentMagnitude = currentVector.magnitude;
+        dotProduct = Vector3.Dot(startVector, currentVector);
+        magnitudes = startMagnitude * currentMagnitude;
+        radians = Mathf.Acos(dotProduct / magnitudes);
+        angle = radians * 180 / Mathf.PI;
+
+        SetCountText();
     }
     void OnAnimatorMove()
     {
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
         m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+    void SetCountText()
+    {
+        currentAngle.text = "Current Angle (from start): " + angle + "º";
     }
 }
